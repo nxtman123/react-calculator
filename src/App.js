9455e0bbe4290e12c2bfa87f6c71330fe1f96816
Kurtis Jantzen <kjjantze@ucalgary.ca>
2019-02-12 18:40:43 -0700

    Use the Paper component instead of a div

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import "./App.css";
+
+import Paper from "@material-ui/core/Paper";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="calc-base">
-        <div className="calc-background">
-
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="calc-frame">
+                <Paper className="calc-paper">
+                </Paper>
+            </div>
+        );
+    }
 }
 
 export default App;
